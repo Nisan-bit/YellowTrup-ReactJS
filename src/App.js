0b29d6418d48a0ea5a9f3931454b56408f76d18d
Nisan Rai <nisan.rai365@gmail.com>
2021-03-18 15:28:50 +0545

    second commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+import First from './First';
+import Second from './Second';
+import Navbar from './components/Navbar'; 
+
+
+function App() {
+  return (
+    <div className="App">
+      {/* <h1>Hello world!</h1> */}
+      {/* <First/> */}
+      {/* <Second/> */}
+      <Navbar/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/First.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const First=()=> {
+    return (
+        <>
+            <p>THis is a REACT JS class, This is our first component build from reac js</p>
+        </>
+    )
+}
+
+export default First
